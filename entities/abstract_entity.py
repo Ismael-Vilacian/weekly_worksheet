9be18305d01b542adef7b6e6b24bfa32416f3009
Ismael Vilacian Gomes <69656180+Ismael-Vilacian@users.noreply.github.com>
2024-03-31 16:46:03 +0000

    Refactor AbstractEntity class to use abstract methods and improve database interaction

--- entities/abstract_entity.py
+++ entities/abstract_entity.py
@@ -1,32 +1,45 @@
+from abc import ABC, abstractmethod
 from database_controller import database
 
-class AbstractEntity:
-    
-    def __init__(self, entity_name, base_structure):
+class AbstractEntity(ABC):
+    def __init__(self):
         self.database_controller = database()
-        self.entity_name = entity_name
-        self.base_structure = base_structure
+        
+        if not self.table_exists():
+            self.create_table()
+
+    @abstractmethod
+    def table_name(self):
+        pass
 
     def create_table(self):
-        try:
-            self.create_table(self.base_structure, self.entity_name)
-        except Exception as e:
-            print(f'Erro ao criar a tabela cursos: {e}')
+        properties_name = self.map_properties(True)
+        self.database_controller.create_table(properties_name, self.table_name())
 
     def add_item(self):
-        try:
-            self.database_controller.add_item(f"'{self.descricao}'", 'descricao', 'Curso')
-        except Exception as e:
-            print(f'Erro ao adicionar item na tabela cursos: {e}')
+        properties_name = self.map_properties()
+        values_properties = self.map_values_properties()
+        self.database_controller.add_item(values_properties, properties_name, self.table_name())
 
-    def remove_item(self):
-        try:
-            self.database_controller.remove_item(f'id = {self.id}', 'Curso')
-        except Exception as e:
-            print(f'Erro ao remover item na tabela cursos: {e}')
+    def table_exists(self):
+        return self.database_controller.table_exists(self.table_name())
 
-    def update_item(self):
-        try:
-            self.database_controller.update_item(f'descricao = {self.descricao}', f'id = {self.id}', 'Curso')
-        except Exception as e:
-            print(f'Erro ao atualizar item na tabela cursos: {e}')
+    def map_properties(self, use_id=False):
+        instance = AbstractEntity()
+        property = ""
+        for property, value in vars(instance).items():
+            if use_id and property == 'id':
+                continue
+            property += f"{property}, "
+
+        return property[:-2]
+    
+    def map_values_properties(self, use_id=False):
+        instance = AbstractEntity()
+        property = ""
+        for property, value in vars(instance).items():
+            if use_id and property == 'id':
+                continue
+            property += f"{value}, "
+
+        return property[:-2]
