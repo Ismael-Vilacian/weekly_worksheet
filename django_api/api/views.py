41bc63f9ca4230614d0f53a06887b8b582368e07
Ismael Vilacian Gomes <69656180+Ismael-Vilacian@users.noreply.github.com>
2024-04-09 01:47:33 +0000

    implements a django rest api

---
+++ django_api/api/views.py
@@ -0,0 +1,6 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(["GET"])
+def hello_world(request):
+    return Response({"message": "Hello, world!"})
